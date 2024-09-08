78e81aaa30f826f9e5d0881989a04067b926496a
Ayaz Nazir <113617975+ayaz2004@users.noreply.github.com>
2024-09-08 13:21:55 +0530

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -4,7 +4,7 @@ const conn = async () => {
   try {
     await mongoose
       .connect(
-        "mongodb+srv://nazirayaz03:wahida2004@cluster0.4zejn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://<********>:<*******>@cluster0.4zejn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
       )
       .then(
         () => {
